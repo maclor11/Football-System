0ff3c24ff4970a639dfdf77c1467e9fd0d847d6d
PatrykKaszny <patrykkaszny@op.pl>
2025-04-12 22:54:09 +0200

    player

--- FootballSystem/src/main/java/com/example/demo/controllers/PlayerController.java
+++ FootballSystem/src/main/java/com/example/demo/controllers/PlayerController.java
@@ -1,17 +1,37 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.Dto.PlayerDto;
 import com.example.demo.services.PlayerService;
 
 @RestController
 public class PlayerController {
-	private PlayerService playerService;
-	
-	@Autowired
-	public PlayerController(PlayerService playerService) {
-		this.playerService = playerService;
-	}
+    private final PlayerService playerService;
+
+    @Autowired
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping("/player")
+    public CollectionModel<PlayerDto> findAllPlayers() {
+        return playerService.findAllPlayers();
+    }
+
+    @GetMapping("/player/{id}")
+    public PlayerDto findPlayerById(@PathVariable Long id) {
+        return playerService.findPlayerById(id);
+    }
+
+    @DeleteMapping("/player/{id}")
+    public void deletePlayerById(@PathVariable Long id) {
+        playerService.deletePlayerById(id);
+    }
 
 }
--- FootballSystem/src/main/java/com/example/demo/services/PlayerService.java
+++ FootballSystem/src/main/java/com/example/demo/services/PlayerService.java
@@ -1,16 +1,39 @@
 package com.example.demo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.Dto.PlayerDto;
+import com.example.demo.model.Player;
 import com.example.demo.repositories.PlayerRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class PlayerService {
-	private final PlayerRepository playerRepository;
-	
-	@Autowired
-	public PlayerService(PlayerRepository playerRepository) {
-		this.playerRepository = playerRepository;
-	}
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public CollectionModel<PlayerDto> findAllPlayers() {
+        List<PlayerDto> playersDTO = StreamSupport.stream(playerRepository.findAll().spliterator(), false)
+                .map(PlayerDto::new)
+                .collect(Collectors.toList());
+        return CollectionModel.of(playersDTO);
+    }
+
+    public PlayerDto findPlayerById(Long id) {
+        Player player = playerRepository.findById(id).orElse(null);
+        return new PlayerDto(player);
+    }
+
+    public void deletePlayerById(Long id) {
+        playerRepository.deleteById(id);
+    }
 }
